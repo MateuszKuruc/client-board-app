731c78ea3ec5482b4e6cd23facd84dbb8adf1383
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-07 22:59:21 +0200

    update configs to handle volt primevue components

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,6 +6,7 @@ import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
+import PrimeVue from 'primevue/config';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,6 +17,9 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                unstyled: true
+            })
             .mount(el);
     },
     progress: {
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: [
+            { find: "@", replacement: path.resolve(__dirname, "./resources/js") },
+            { find: "@volt", replacement: path.resolve(__dirname, "./src/volt") }
+        ]
+    },
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
