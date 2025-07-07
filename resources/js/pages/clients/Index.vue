46020e395f95fda829dfac04e7096bdb2ab79f05
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-07 23:10:12 +0200

    work on index page setup for clients, add handling of nested data with project and services info for clients

--- resources/js/pages/clients/Index.vue
+++ resources/js/pages/clients/Index.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
-import { Head } from '@inertiajs/vue3';
 import type { BreadcrumbItem } from '@/types';
-import DataTable from '@volt/DataTable.vue';
-import Column from 'primevue/column';
-import ColumnGroup from 'primevue/columngroup';  // optional
-import Row from 'primevue/row';          // optional
-import { ref } from 'vue';
+import { Head } from '@inertiajs/vue3';
 import { FilterMatchMode } from '@primevue/core/api';
+import DataTable from '@volt/DataTable.vue';
 import InputText from '@volt/InputText.vue';
-import { Link } from '@inertiajs/vue3'
 import SecondaryButton from '@volt/SecondaryButton.vue';
+import Column from 'primevue/column';
+import { ref } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -20,11 +17,11 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 defineProps({
-    clients: Array
-})
+    clients: Array,
+});
 
 const filters = ref({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
 });
 </script>
 
@@ -32,17 +29,20 @@ const filters = ref({
     <Head title="Klienci" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div>
-            <p>yoooo test</p>
-            <Link :href="route('clients.index')">Gunwo</Link>
-        </div>
-
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-            <DataTable v-model:filters="filters" :globalFilterFields="['name', 'email', 'phone', 'NIP']" :value="clients" paginator :rows="5" sortMode="multiple" pt:table="min-w-200">
+        <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <DataTable
+                v-model:filters="filters"
+                :globalFilterFields="['name', 'email', 'phone', 'NIP']"
+                :value="clients"
+                paginator
+                :rows="5"
+                sortMode="multiple"
+                pt:table="min-w-200"
+            >
                 <template #header>
                     <div class="flex justify-end">
                         <div class="relative">
-                            <i class="pi pi-search absolute top-1/2 -mt-2 text-surface-400 leading-none end-3 z-1" />
+                            <i class="pi pi-search absolute end-3 top-1/2 z-1 -mt-2 leading-none text-surface-400" />
                             <InputText v-model="filters['global'].value" placeholder="Search" />
                         </div>
                     </div>
@@ -51,9 +51,21 @@ const filters = ref({
                 <Column field="email" header="Email" sortable></Column>
                 <Column field="phone" header="Telefon" sortable></Column>
                 <Column field="NIP" header="NIP" sortable></Column>
+                <Column header="Usługi">
+                    <template #body="{ data }">
+                        <div v-if="data.projects.length > 0">
+                            <ul>
+                                <li v-for="project in data.projects" :key="project.id">
+                                    {{ project.service.name }}
+                                </li>
+                            </ul>
+                        </div>
+                        <span v-else>-</span>
+                    </template>
+                </Column>
                 <Column>
                     <template #body="{ data }">
-                            <SecondaryButton label="Edit" />
+                        <SecondaryButton label="Edit" />
                     </template>
                 </Column>
                 <Column></Column>
@@ -61,4 +73,3 @@ const filters = ref({
         </div>
     </AppLayout>
 </template>
-
