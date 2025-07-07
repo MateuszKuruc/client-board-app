5987ead8fbf39b3dbd3989a322e744afb59a13ba
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-07 23:00:35 +0200

    setup index method for client controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        $clients = Client::with('projects.service')->get();
+
+        return Inertia::render('clients/Index', [
+            'clients' => $clients
+        ]);
+    }
 }
