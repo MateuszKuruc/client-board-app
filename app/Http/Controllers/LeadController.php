a0a002fc611c42bf3e0f0232a34dd6519254a67b
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-09 01:31:57 +0200

    setup lead controller to handle search

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class LeadController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+
+        $leads = Lead::when($search, function ($query) use ($search) {
+             $query->where('email', 'like', '%' . $search . '%')
+                 ->orWhere('phone', 'like', '%' . $search . '%');
+        })
+            ->paginate(10)
+            ->withQueryString();
+
+        return Inertia::render('/leads/Index', [
+            'leads' => $leads,
+            'filters' => [
+                'search' => $search,
+            ]
+        ]);
+    }
 }
