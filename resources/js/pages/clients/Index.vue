6e3e63d6a14c7632d86d56018f5a8a3505cb6dea
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-08 16:25:21 +0200

    add conditional rendering of projects history nested data table

--- resources/js/pages/clients/Index.vue
+++ resources/js/pages/clients/Index.vue
@@ -8,6 +8,7 @@ import ContrastButton from '@volt/ContrastButton.vue';
 import DataTable from '@volt/DataTable.vue';
 import InputText from '@volt/InputText.vue';
 import SecondaryButton from '@volt/SecondaryButton.vue';
+import Tag from '@volt/Tag.vue';
 import { debounce } from 'lodash';
 import { ChevronsDown, ChevronsUp, Circle, Search, User } from 'lucide-vue-next';
 import Column from 'primevue/column';
@@ -101,34 +102,43 @@ watch(globalSearch, (value) => {
                 </Column>
                 <Column field="email" header="Email" sortable></Column>
                 <Column field="phone" header="Telefon"></Column>
-                <Column field="NIP" header="NIP"></Column>
+                <Column field="nip" header="NIP"></Column>
 
                 <template #expansion="{ data }">
                     <div class="space-y-4 bg-gray-50 p-4">
                         <h5 class="font-semibold">Historia projektów</h5>
 
                         <DataTable :value="data.projects" dataKey="id" scrollable scrollHeight="200px" removableSort>
-                            <Column field="name" header="Nazwa projektu" sortable />
-                            <Column field="service.name" header="Usługa" sortable />
-                            <Column field="start_date" header="Data startu" sortable />
-                            <Column field="end_date" header="Data końca" sortable />
-                            <Column field="price" header="Cena" sortable>
-                                <template #body="{ data: project }"> {{ Number(project.price).toFixed(2) }} zł </template>
-                            </Column>
-                            <Column field="active" header="Aktywny" sortable>
-                                <template #body="{ data: project }">
-                                    <span :class="project.active ? 'text-green-600' : 'text-red-600'">
-                                        {{ project.active ? 'Tak' : 'Nie' }}
-                                    </span>
-                                </template>
-                            </Column>
-                            <Column header="Akcja">
-                                <template #body="{ data: project }">
-                                    <Link :href="route('projects.show', { client: project.client.slug, project: project.id })">
-                                        <ContrastButton icon="pi pi-search" severity="info" label="Szczegóły projektu" />
-                                    </Link>
-                                </template>
-                            </Column>
+                            <div v-if="data.projects.length > 0">
+                                <Column field="name" header="Nazwa projektu" sortable />
+                                <Column field="service.name" header="Usługa" sortable>
+                                    <template #body="{ data }">
+                                        <Tag :value="data.service.name" severity="info"></Tag>
+                                    </template>
+                                </Column>
+                                <Column field="start_date" header="Data startu" sortable />
+                                <Column field="end_date" header="Data końca" sortable />
+                                <Column field="price" header="Cena" sortable>
+                                    <template #body="{ data: project }"> {{ Number(project.price).toFixed(2) }} zł </template>
+                                </Column>
+                                <Column field="active" header="Aktywny" sortable>
+                                    <template #body="{ data: project }">
+                                        <span :class="project.active ? 'text-green-600' : 'text-red-600'">
+                                            {{ project.active ? 'Tak' : 'Nie' }}
+                                        </span>
+                                    </template>
+                                </Column>
+                                <Column header="Akcja">
+                                    <template #body="{ data: project }">
+                                        <Link :href="route('projects.show', { client: project.client.slug, project: project.id })">
+                                            <ContrastButton icon="pi pi-search" severity="info" label="Szczegóły projektu" />
+                                        </Link>
+                                    </template>
+                                </Column>
+                            </div>
+                            <div v-else>
+                                <p>Żadne projekty nie zostały zrealizowane.</p>
+                            </div>
                         </DataTable>
                     </div>
                 </template>
