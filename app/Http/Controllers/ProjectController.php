3e77e9359962ff220f2a1f2ee0dfcae893e75a7b
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-08 18:54:17 +0200

    setup project controller to pass paginated projects data to inertia

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::with('client', 'service', 'payments')->paginate(10);
+
+        return Inertia::render('projects/Index', [
+            'projects' => $projects,
+        ]);
+    }
 }
