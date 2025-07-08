a254dd81867f5b4fd7f80b9c936829549463b394
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-09 01:23:49 +0200

    setup payments controller to pass paginated payments data and handle server side search logic

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payment;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class PaymentController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+
+        $payments = Payment::with('project', 'project.client')
+            ->when($search, function ($query, $search) {
+                $query->where(function ($query) use ($search) {
+                    $query->whereHas('project', function ($q) use ($search) {
+                        $q->where('name', 'like', '%' . $search . '%');
+                    })->orWhereHas('project.client', function ($q) use ($search) {
+                        $q->where('name', 'like', '%' . $search . '%');
+                    });
+                });
+            })
+            ->paginate(10)
+            ->withQueryString();
+
+        return Inertia::render('payments/Index', [
+            'payments' => $payments,
+            'filters' => [
+                'search' => $search,
+            ]
+        ]);
+    }
 }
