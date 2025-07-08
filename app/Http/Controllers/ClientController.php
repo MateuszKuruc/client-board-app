68c9dfa98c0f36f7c9d0b4a9142a89043d907b55
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-08 15:20:39 +0200

    add server side filtering for clients

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,12 +8,23 @@ use Inertia\Inertia;
 
 class ClientController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $clients = Client::with('projects.service', 'projects.client')->get();
+        $search = $request->input('search');
+
+        $clients = Client::with(['projects.service', 'projects.client'])
+            ->when($search, function ($query, $search) {
+                $query->where('name', 'like', '%' . $search . '%')
+                    ->orWhere('email', 'like', '%' . $search . '%');
+            })
+            ->paginate(10)
+            ->withQueryString();
 
         return Inertia::render('clients/Index', [
-            'clients' => $clients
+            'clients' => $clients,
+            'filters' => [
+                'search' => $search,
+            ],
         ]);
     }
 }
