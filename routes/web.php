8b1cd73ec951d25b1d1855b5b057f69dc9e85b49
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-09 20:50:21 +0200

    update routes to include export routes for different tables

--- routes/web.php
+++ routes/web.php
@@ -22,44 +22,47 @@ Route::middleware(['auth'])->group(function () {
     // Home
     Route::get('/pulpit', [DashboardController::class, 'index'])->name('dashboard.index');
 
-   // Clients
-   Route::get('/klienci', [ClientController::class, 'index'])->name('clients.index');
-   Route::get('/klienci/dodaj', [ClientController::class, 'create'])->name('clients.create');
-   Route::get('/klienci/{client}', [ClientController::class, 'show'])->name('clients.show');
-   Route::get('/klienci/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
-   Route::put('/klienci/{client}', [ClientController::class, 'update'])->name('clients.update');
-   Route::post('/klienci', [ClientController::class, 'store'])->name('clients.store');
+    // Clients
+    Route::get('/klienci', [ClientController::class, 'index'])->name('clients.index');
+    Route::get('/klienci/dodaj', [ClientController::class, 'create'])->name('clients.create');
+    Route::get('/klienci/{client}', [ClientController::class, 'show'])->name('clients.show');
+    Route::get('/klienci/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+    Route::put('/klienci/{client}', [ClientController::class, 'update'])->name('clients.update');
+    Route::post('/klienci', [ClientController::class, 'store'])->name('clients.store');
+    Route::get('/klienci/eksport', [ClientController::class, 'export'])->name('clients.export');
 
-   //  Projects
+    //  Projects
     Route::prefix('/klienci/{client}')->group(function () {
         Route::get('/projekty/{project}', [ProjectController::class, 'show'])->name('projects.show');
         Route::get('/projekty/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
     });
-   Route::get('/projekty', [ProjectController::class, 'index'])->name('projects.index');
-   Route::get('/projekty/dodaj', [ProjectController::class, 'create'])->name('projects.create');
+    Route::get('/projekty', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projekty/dodaj', [ProjectController::class, 'create'])->name('projects.create');
+    Route::get('/projekty/eksport', [ProjectController::class, 'export'])->name('projects.export');
 
 
-   Route::prefix('/projekty/{project}')->group(function () {
-       Route::get('/platnosci/{payment}', [PaymentController::class, 'show'])->name('payments.show');
-       Route::get('/platnosci/{payment}/edit', [PaymentController::class, 'edit'])->name('payments.edit');
-   });
+    Route::prefix('/projekty/{project}')->group(function () {
+        Route::get('/platnosci/{payment}', [PaymentController::class, 'show'])->name('payments.show');
+        Route::get('/platnosci/{payment}/edit', [PaymentController::class, 'edit'])->name('payments.edit');
+    });
 
 
-   //  Payments
-     Route::get('/platnosci', [PaymentController::class, 'index'])->name('payments.index');
+    //  Payments
+    Route::get('/platnosci', [PaymentController::class, 'index'])->name('payments.index');
+    Route::get('/platnosci/eksport', [PaymentController::class, 'export'])->name('payments.export');
 
 
     // Expenses
-     Route::get('/koszty', [ExpenseController::class, 'index'])->name('expenses.index');
-     Route::get('/koszty/dodaj', [ExpenseController::class, 'create'])->name('expenses.create');
-     Route::put('/koszty/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
-     Route::post('/koszty', [ExpenseController::class, 'store'])->name('expenses.store');
+    Route::get('/koszty', [ExpenseController::class, 'index'])->name('expenses.index');
+    Route::get('/koszty/dodaj', [ExpenseController::class, 'create'])->name('expenses.create');
+    Route::put('/koszty/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+    Route::post('/koszty', [ExpenseController::class, 'store'])->name('expenses.store');
 
-     // Leads
+    // Leads
     Route::get('/leady', [LeadController::class, 'index'])->name('leads.index');
     Route:: get('/leady/dodaj', [LeadController::class, 'create'])->name('leads.create');
     Route::put('/leady/{lead}', [LeadController::class, 'update'])->name('leads.update');
-    Route::post('/leady',  [LeadController::class, 'store'])->name('leads.store');
+    Route::post('/leady', [LeadController::class, 'store'])->name('leads.store');
     Route::get('/leady/eksport', [LeadController::class, 'export'])->name('leads.export');
 
     // Configuration
