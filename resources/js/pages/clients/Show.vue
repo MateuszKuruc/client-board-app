346b2199aae74fb6dd67ec801a7ec2df3a2e9b6d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-10 17:19:48 +0200

    clean code, make use of reusable components, add data tables and handle props

--- resources/js/pages/clients/Show.vue
+++ resources/js/pages/clients/Show.vue
@@ -2,8 +2,11 @@
 import EditableField from '@/components/EditableField.vue';
 import PageHeading from '@/components/PageHeading.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
+import SectionHeading from '@/pages/clients/SectionHeading.vue';
 import type { BreadcrumbItem } from '@/types';
 import { Head, useForm } from '@inertiajs/vue3';
+import DataTable from '@volt/DataTable.vue';
+import Column from 'primevue/column';
 import { ref } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -31,12 +34,27 @@ const form = useForm({
 
 const isEditing = ref(false);
 
+const sourceOptions = ['Strona internetowa', 'Social media', 'Polecenie', 'Ads', 'Grupki', 'Useme', 'Inne'];
+
+const editableFields = [
+    { key: 'name', label: 'Nazwa' },
+    { key: 'email', label: 'Email' },
+    { key: 'phone', label: 'Telefon' },
+    { key: 'nip', label: 'NIP' },
+    {
+        key: 'source',
+        label: 'Źródło',
+        type: 'select',
+        options: sourceOptions,
+    },
+];
+
 function submitEdit() {
     form.put(route('clients.update', props.client.slug), {
-       preserveScroll: true,
+        preserveScroll: true,
         onSuccess: () => {
-           isEditing.value = false;
-        }
+            isEditing.value = false;
+        },
     });
 }
 
@@ -60,74 +78,116 @@ function cancelEdit() {
             <!--PROFILE -->
 
             <div>
-                <div class="px-4 sm:px-0">
-                    <h3 class="text-base/7 font-semibold text-gray-900">Informacje o kliencie</h3>
-                    <p class="mt-1 max-w-2xl text-sm/6 text-gray-500">Pełny profil klienta.</p>
-                </div>
+                <SectionHeading heading="Informacje o kliencie" subheading="Pełny profil klienta" />
                 <div class="mt-6 border-t border-gray-100">
-                    <dl class="divide-y divide-gray-100">
-                        <EditableField v-model="form.name" :isEditing="isEditing" label="Nazwa" />
-                        <EditableField v-model="form.email" :isEditing="isEditing" label="Email" />
-                        <EditableField v-model="form.phone" :isEditing="isEditing" label="Telefon" />
-                        <EditableField v-model="form.nip" :isEditing="isEditing" label="NIP" />
-                        <EditableField v-model="form.source" :isEditing="isEditing" label="Źródło" />
+                    <ul class="divide-y divide-gray-100">
+                        <li v-for="field in editableFields" :key="field.key">
+                            <EditableField
+                                v-model="form[field.key]"
+                                :isEditing="isEditing"
+                                :label="field.label"
+                                :type="field.type || 'text'"
+                                :options="field.options || []"
+                            />
+                        </li>
+                    </ul>
 
-                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt class="text-sm/6 font-medium text-gray-900">Attachments</dt>
-                            <dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                <ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
-                                    <li class="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
-                                        <div class="flex w-0 flex-1 items-center">
-                                            <svg
-                                                class="size-5 shrink-0 text-gray-400"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                                aria-hidden="true"
-                                                data-slot="icon"
-                                            >
-                                                <path
-                                                    fill-rule="evenodd"
-                                                    d="M15.621 4.379a3 3 0 0 0-4.242 0l-7 7a3 3 0 0 0 4.241 4.243h.001l.497-.5a.75.75 0 0 1 1.064 1.057l-.498.501-.002.002a4.5 4.5 0 0 1-6.364-6.364l7-7a4.5 4.5 0 0 1 6.368 6.36l-3.455 3.553A2.625 2.625 0 1 1 9.52 9.52l3.45-3.451a.75.75 0 1 1 1.061 1.06l-3.45 3.451a1.125 1.125 0 0 0 1.587 1.595l3.454-3.553a3 3 0 0 0 0-4.242Z"
-                                                    clip-rule="evenodd"
-                                                />
-                                            </svg>
-                                            <div class="ml-4 flex min-w-0 flex-1 gap-2">
-                                                <span class="truncate font-medium">resume_back_end_developer.pdf</span>
-                                                <span class="shrink-0 text-gray-400">2.4mb</span>
-                                            </div>
-                                        </div>
-                                        <div class="ml-4 shrink-0">
-                                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
-                                        </div>
-                                    </li>
-                                    <li class="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
-                                        <div class="flex w-0 flex-1 items-center">
-                                            <svg
-                                                class="size-5 shrink-0 text-gray-400"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                                aria-hidden="true"
-                                                data-slot="icon"
-                                            >
-                                                <path
-                                                    fill-rule="evenodd"
-                                                    d="M15.621 4.379a3 3 0 0 0-4.242 0l-7 7a3 3 0 0 0 4.241 4.243h.001l.497-.5a.75.75 0 0 1 1.064 1.057l-.498.501-.002.002a4.5 4.5 0 0 1-6.364-6.364l7-7a4.5 4.5 0 0 1 6.368 6.36l-3.455 3.553A2.625 2.625 0 1 1 9.52 9.52l3.45-3.451a.75.75 0 1 1 1.061 1.06l-3.45 3.451a1.125 1.125 0 0 0 1.587 1.595l3.454-3.553a3 3 0 0 0 0-4.242Z"
-                                                    clip-rule="evenodd"
-                                                />
-                                            </svg>
-                                            <div class="ml-4 flex min-w-0 flex-1 gap-2">
-                                                <span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
-                                                <span class="shrink-0 text-gray-400">4.5mb</span>
-                                            </div>
-                                        </div>
-                                        <div class="ml-4 shrink-0">
-                                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </dd>
-                        </div>
-                    </dl>
+                    <!--                        Aktywne projekty -->
+
+                    <div class="flex flex-col gap-12">
+
+
+                    <div class="mt-6">
+                        <SectionHeading heading="Lista aktywnych projektów" subheading="Śledź projekty, które są aktualnie w realizacji" />
+                        <DataTable class="mt-6" :value="client.projects" dataKey="id">
+                            <Column field="name" header="Projekt" />
+                            <Column field="service.name" header="Usługa" />
+                            <Column field="start_date" header="Data startu" />
+                            <Column field="end_date" header="Data zakończenia" />
+                            <Column field="price" header="Cena" />
+                            <Column field="active" header="Aktywny">
+                                <template #body="{ data }">
+                                    <span class='text-green-600'>
+                                        Tak
+                                    </span>
+                                </template>
+                            </Column>
+                        </DataTable>
+                    </div>
+                    <!--                        Nieaktywne projekty -->
+                    <div class="mt-6">
+                        <SectionHeading heading="Lista nieaktywnych projektów" subheading="Sprawdź zakończone oraz anulowane projekty" />
+                        <DataTable class="mt-6" :value="client.projects" dataKey="id">
+                            <Column field="name" header="Projekt" />
+                            <Column field="service.name" header="Usługa" />
+                            <Column field="start_date" header="Data startu" />
+                            <Column field="end_date" header="Data zakończenia" />
+                            <Column field="price" header="Cena" />
+                            <Column field="active" header="Aktywny">
+                                <template #body="{ data }">
+                                    <span v-if="!data.active" class="text-red-600">
+                                        Nie
+                                    </span>
+                                </template>
+                            </Column>
+                        </DataTable>
+                    </div>
+                    </div>
+                    <!--                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">-->
+                    <!--                            <dt class="text-sm/6 font-medium text-gray-900">Attachments</dt>-->
+                    <!--                            <dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">-->
+                    <!--                                <ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">-->
+                    <!--                                    <li class="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">-->
+                    <!--                                        <div class="flex w-0 flex-1 items-center">-->
+                    <!--                                            <svg-->
+                    <!--                                                class="size-5 shrink-0 text-gray-400"-->
+                    <!--                                                viewBox="0 0 20 20"-->
+                    <!--                                                fill="currentColor"-->
+                    <!--                                                aria-hidden="true"-->
+                    <!--                                                data-slot="icon"-->
+                    <!--                                            >-->
+                    <!--                                                <path-->
+                    <!--                                                    fill-rule="evenodd"-->
+                    <!--                                                    d="M15.621 4.379a3 3 0 0 0-4.242 0l-7 7a3 3 0 0 0 4.241 4.243h.001l.497-.5a.75.75 0 0 1 1.064 1.057l-.498.501-.002.002a4.5 4.5 0 0 1-6.364-6.364l7-7a4.5 4.5 0 0 1 6.368 6.36l-3.455 3.553A2.625 2.625 0 1 1 9.52 9.52l3.45-3.451a.75.75 0 1 1 1.061 1.06l-3.45 3.451a1.125 1.125 0 0 0 1.587 1.595l3.454-3.553a3 3 0 0 0 0-4.242Z"-->
+                    <!--                                                    clip-rule="evenodd"-->
+                    <!--                                                />-->
+                    <!--                                            </svg>-->
+                    <!--                                            <div class="ml-4 flex min-w-0 flex-1 gap-2">-->
+                    <!--                                                <span class="truncate font-medium">resume_back_end_developer.pdf</span>-->
+                    <!--                                                <span class="shrink-0 text-gray-400">2.4mb</span>-->
+                    <!--                                            </div>-->
+                    <!--                                        </div>-->
+                    <!--                                        <div class="ml-4 shrink-0">-->
+                    <!--                                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>-->
+                    <!--                                        </div>-->
+                    <!--                                    </li>-->
+                    <!--                                    <li class="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">-->
+                    <!--                                        <div class="flex w-0 flex-1 items-center">-->
+                    <!--                                            <svg-->
+                    <!--                                                class="size-5 shrink-0 text-gray-400"-->
+                    <!--                                                viewBox="0 0 20 20"-->
+                    <!--                                                fill="currentColor"-->
+                    <!--                                                aria-hidden="true"-->
+                    <!--                                                data-slot="icon"-->
+                    <!--                                            >-->
+                    <!--                                                <path-->
+                    <!--                                                    fill-rule="evenodd"-->
+                    <!--                                                    d="M15.621 4.379a3 3 0 0 0-4.242 0l-7 7a3 3 0 0 0 4.241 4.243h.001l.497-.5a.75.75 0 0 1 1.064 1.057l-.498.501-.002.002a4.5 4.5 0 0 1-6.364-6.364l7-7a4.5 4.5 0 0 1 6.368 6.36l-3.455 3.553A2.625 2.625 0 1 1 9.52 9.52l3.45-3.451a.75.75 0 1 1 1.061 1.06l-3.45 3.451a1.125 1.125 0 0 0 1.587 1.595l3.454-3.553a3 3 0 0 0 0-4.242Z"-->
+                    <!--                                                    clip-rule="evenodd"-->
+                    <!--                                                />-->
+                    <!--                                            </svg>-->
+                    <!--                                            <div class="ml-4 flex min-w-0 flex-1 gap-2">-->
+                    <!--                                                <span class="truncate font-medium">coverletter_back_end_developer.pdf</span>-->
+                    <!--                                                <span class="shrink-0 text-gray-400">4.5mb</span>-->
+                    <!--                                            </div>-->
+                    <!--                                        </div>-->
+                    <!--                                        <div class="ml-4 shrink-0">-->
+                    <!--                                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>-->
+                    <!--                                        </div>-->
+                    <!--                                    </li>-->
+                    <!--                                </ul>-->
+                    <!--                            </dd>-->
+                    <!--                        </div>-->
                 </div>
             </div>
         </div>
