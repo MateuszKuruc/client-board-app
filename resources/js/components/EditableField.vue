4f5a0f564f6b2d72d758bd9426710863d1b7e1d8
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-10 17:19:01 +0200

    add option for select dropdown conditional rendering

--- resources/js/components/EditableField.vue
+++ resources/js/components/EditableField.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
 import InputText from '@volt/InputText.vue';
+import Select from '@volt/Select.vue';
+import { ref, watch} from 'vue';
 
-
-defineProps({
+const props = defineProps({
     label: String,
     isEditing: Boolean,
     modelValue: String,
+    type: {
+        type: String,
+        default: 'text',
+    },
+    options: {
+        type: Array,
+        default: () => [],
+    },
 });
+
+const internalValue = ref(props.modelValue);
+
+// Watch for changes and emit updated value
+watch(internalValue, val => emit('update:modelValue', val));
 
 const emit = defineEmits(['update:modelValue']);
 </script>
@@ -15,13 +29,17 @@ const emit = defineEmits(['update:modelValue']);
     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
         <label class="text-sm/6 font-medium text-gray-900">{{ label }}</label>
         <p v-if="!isEditing" class="mt-1 px-2 py-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">{{ modelValue }}</p>
+        <Select
+            v-if="isEditing && type === 'select'"
+            :options="options"
+            v-model="internalValue"
+        />
         <InputText
-            v-if="isEditing"
+            v-else-if="isEditing"
             type="text"
             :value="modelValue"
-            @input="(event) => emit('update:modelValue', event.target.value)"
+            @input="(e) => emit('update:modelValue', e.target.value)"
             class="mt-1 px-2 py-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0"
         />
-
     </div>
 </template>
