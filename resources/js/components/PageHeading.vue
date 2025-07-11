341637d67d93f96cbab42bc246869b4f545bde42
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-11 18:21:25 +0200

    add dynamic info on first and latest payments

--- resources/js/components/PageHeading.vue
+++ resources/js/components/PageHeading.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import Button from '@/components/volt/Button.vue';
-import SecondaryButton from '@/components/volt/SecondaryButton.vue';
+import dayjs from '@/plugins/dayjs';
+import { computed } from 'vue';
 
-defineProps({
+const props = defineProps({
     title: String,
     isEditing: Boolean,
+    client: Object,
 });
 
 const emit = defineEmits(['cancel', 'edit', 'save']);
@@ -20,6 +21,24 @@ function onEdit() {
 function onSave() {
     emit('save');
 }
+
+const firstPaidProjectDate = computed(() => {
+    const paidPayments = props.client.projects
+        .flatMap((p) => p.payments.filter((payment) => payment.status === 'paid'))
+        .sort((a, b) => new Date(a.payment_date) - new Date(b.payment_date));
+
+    return paidPayments[0]?.payment_date || null;
+});
+
+const lastPaidProjectEndDate = computed(() => {
+    const paidPaymentsWithProject = props.client.projects.flatMap((project) =>
+        project.payments.filter((payment) => payment.status === 'paid').map((payment) => ({ ...payment, project })),
+    );
+
+    const sorted = paidPaymentsWithProject.sort((a, b) => new Date(a.payment_date) - new Date(b.payment_date));
+
+    return sorted.at(-1)?.project?.end_date || null;
+});
 </script>
 
 <template>
@@ -38,7 +57,7 @@ function onSave() {
                             d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 0 1-9.274 0C3.985 17.585 3 16.402 3 15.055Z"
                         />
                     </svg>
-                    Full-time
+                    {{ client.projects.some((p) => p.active) ? 'Aktywny' : 'Nieaktywny' }}
                 </div>
                 <div class="mt-2 flex items-center text-sm text-gray-500">
                     <svg class="mr-1.5 size-5 shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
@@ -48,7 +67,7 @@ function onSave() {
                             clip-rule="evenodd"
                         />
                     </svg>
-                    Remote
+                    {{ client.location === 'local' ? 'Lokalny' : client.location === 'remote' ? 'Krajowy' : 'Zagraniczny' }}
                 </div>
                 <div class="mt-2 flex items-center text-sm text-gray-500">
                     <svg class="mr-1.5 size-5 shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
@@ -61,7 +80,7 @@ function onSave() {
                             clip-rule="evenodd"
                         />
                     </svg>
-                    $120k &ndash; $140k
+                    Subskrypcja + dodatkowe
                 </div>
                 <div class="mt-2 flex items-center text-sm text-gray-500">
                     <svg class="mr-1.5 size-5 shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
@@ -71,55 +90,19 @@ function onSave() {
                             clip-rule="evenodd"
                         />
                     </svg>
-                    Closing on January 9, 2020
+                    Klient od {{ dayjs(firstPaidProjectDate).format('DD.MM.YYYY') || 'Brak danych' }}
                 </div>
-            </div>
-        </div>
-        <div class="mt-5 flex gap-1 lg:mt-0 lg:ml-4">
-            <span>
-                <SecondaryButton
-                    v-if="!isEditing"
-                    @click="onEdit"
-                    class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50"
-                >
-                    <svg class="mr-1.5 -ml-0.5 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                        <path
-                            d="m2.695 14.762-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z"
-                        />
-                    </svg>
-                    Edytuj
-                </SecondaryButton>
-
-                <SecondaryButton
-                    v-if="isEditing"
-                    @click="onCancel"
-                    class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50"
-                >
-                    <svg class="mr-1.5 -ml-0.5 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                        <path
-                            d="m2.695 14.762-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z"
-                        />
-                    </svg>
-                    Anuluj
-                </SecondaryButton>
-            </span>
-
-            <span class="sm:ml-3">
-                <Button
-                    v-if="isEditing"
-                    @click="onSave"
-                    class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    <svg class="mr-1.5 -ml-0.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                <div class="mt-2 flex items-center text-sm text-gray-500">
+                    <svg class="mr-1.5 size-5 shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                         <path
                             fill-rule="evenodd"
-                            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                            d="M5.75 2a.75.75 0 0 1 .75.75V4h7V2.75a.75.75 0 0 1 1.5 0V4h.25A2.75 2.75 0 0 1 18 6.75v8.5A2.75 2.75 0 0 1 15.25 18H4.75A2.75 2.75 0 0 1 2 15.25v-8.5A2.75 2.75 0 0 1 4.75 4H5V2.75A.75.75 0 0 1 5.75 2Zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75Z"
                             clip-rule="evenodd"
                         />
                     </svg>
-                    Zapisz
-                </Button>
-            </span>
+                    Opłacony do {{ dayjs(lastPaidProjectEndDate).format('DD.MM.YYYY') || 'Brak danych' }}
+                </div>
+            </div>
         </div>
     </div>
 </template>
