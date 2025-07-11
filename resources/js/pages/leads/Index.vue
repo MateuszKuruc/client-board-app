ba97d7f9d6ae904a6b599d98ef3abfc03d1554ae
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-12 00:46:14 +0200

    change components to use typescript

--- resources/js/pages/leads/Index.vue
+++ resources/js/pages/leads/Index.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import Paginator from '@/components/Paginator.vue';
-import { useServerSearch } from '@/composables/useServerSearch';
-import AppLayout from '@/layouts/AppLayout.vue';
 import DataTableToolbar from '@/components/DataTableToolbar.vue';
-import type { BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import Paginator from '@/components/Paginator.vue';
 import Button from '@/components/volt/Button.vue';
 import DataTable from '@/components/volt/DataTable.vue';
 import Tag from '@/components/volt/Tag.vue';
+import { useServerSearch } from '@/composables/useServerSearch';
+import AppLayout from '@/layouts/AppLayout.vue';
+import type { BreadcrumbItem } from '@/types';
+import type { Filters, Lead, Paginated } from '@/types/models';
+import { Head } from '@inertiajs/vue3';
 import { SquarePen } from 'lucide-vue-next';
 import Column from 'primevue/column';
-import type { Lead, Paginated, Filters } from '@/types/models';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -19,11 +19,10 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const { leads, filters} = defineProps<{
+const { leads, filters } = defineProps<{
     leads: Paginated<Lead>;
-    filters: Filters
+    filters: Filters;
 }>();
-
 
 const { globalSearch } = useServerSearch(filters.search || '', 'leads.index');
 </script>
@@ -40,18 +39,18 @@ const { globalSearch } = useServerSearch(filters.search || '', 'leads.index');
 
                 <Column field="email" header="Email"> </Column>
                 <Column field="phone" header="Telefon">
-                    <template #body="{ data }">
+                    <template #body="{ data }: { data: Lead }">
                         {{ data.phone ? data.phone : '-' }}
                     </template>
                 </Column>
                 <Column field="converted_at" header="Status">
-                    <template #body="{ data }">
+                    <template #body="{ data }: { data: Lead }">
                         <Tag :value="data.converted_at ? 'Przekonwertowany' : 'Potencjalny'" :severity="data.converted_at ? 'success' : 'warn'" />
                     </template>
                 </Column>
 
                 <Column header="Aktualizacja">
-                    <template #body="{ data }">
+                    <template #body="{ data }: { data: Lead }">
                         <!--                        <Link :href="route('projects.show', { client: data.client.slug, project: data.id })">-->
                         <Button><SquarePen />Zamień w klienta</Button>
                         <!--                        </Link>-->
