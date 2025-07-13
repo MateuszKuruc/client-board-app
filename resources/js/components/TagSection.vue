f5f8e508efb3f8bdc49a4a6452fc9ee805bca205
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-13 16:36:12 +0200

    add temporary tag section

---
+++ resources/js/components/TagSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import SecondaryButton from '@/components/volt/SecondaryButton.vue';
+import Tag from '@/components/volt/Tag.vue';
+import { Plus } from 'lucide-vue-next';
+</script>
+
+<template>
+    <div class="flex items-center justify-between">
+        <div class="flex w-[600px] flex-wrap gap-2">
+            <Tag value="VIP" severity="primary" />
+            <Tag value="Problematyczny" severity="secondary" />
+            <Tag value="Problemy z płatnościami" severity="danger" />
+            <Tag value="Długoterminowa współpraca" severity="info" />
+            <Tag value="Kontakt przez telefon" severity="warn" />
+            <Tag value="Kontakt przez e-mail" severity="danger" />
+            <Tag value="Testowy" severity="contrast" />
+        </div>
+        <SecondaryButton>
+            <Plus class="w-5 text-gray-400" />
+            Edytuj tagi</SecondaryButton
+        >
+    </div>
+</template>
