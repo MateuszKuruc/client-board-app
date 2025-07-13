366349b79dd1a57ec598fd99944284a5a8953a85
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-13 10:48:23 +0200

    add interfaces for models with their relationships

--- resources/js/types/models.ts
+++ resources/js/types/models.ts
@@ -8,6 +8,61 @@ export interface Lead {
     updated_at: string;
 }
 
+export type Source = 'Strona internetowa' | 'Social media' | 'Polecenie' | 'Ads' | 'Grupki' | 'Useme' | 'Inne';
+
+export type Location = 'local' | 'remote' | 'international';
+export interface Client {
+    id: number;
+    name: string;
+    slug: string;
+    email: string;
+    phone: string | null;
+    nip: string;
+    source: Source | null;
+    location: Location | null;
+    created_at: string;
+    updated_at: string;
+
+    projects?: Project[];
+}
+
+export interface Payment {
+    id: number;
+    project_id: number;
+    amount: number;
+    status: string;
+    payment_date: string;
+    created_at: string;
+    updated_at: string;
+
+    project?: Project;
+}
+
+export interface Project {
+    id: number;
+    name: string;
+    client_id: number;
+    service_id: number;
+    active: boolean;
+    price: number;
+    type: string;
+    start_date: string;
+    end_date: string;
+    created_at: string;
+    updated_at: string;
+
+    payments?: Payment[];
+    service?: Service;
+    client?: Client;
+}
+
+export interface Service {
+    id: number;
+    name: string;
+    created_at: string;
+    updated_at: string;
+}
+
 export interface Paginated<T> {
     current_page: number;
     data: T[];
@@ -19,8 +74,8 @@ export interface Paginated<T> {
         active: boolean;
         label: string;
         url: string | null;
-    }[],
-    next_page_url: string |null;
+    }[];
+    next_page_url: string | null;
     path: string;
     per_page: number;
     prev_page_url: string | null;
