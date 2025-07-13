90c397d8400a25dd7184dac889d7c7eb0b886c33
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-13 16:34:59 +0200

    create reusable component for cards to use in dashboards

---
+++ resources/js/pages/clients/ReusableCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Card from '@/components/volt/Card.vue';
+
+const { value } = defineProps<{
+    heading: string;
+    subheading?: string;
+    value: number;
+    secondValue?: number;
+}>();
+</script>
+
+<template>
+    <Card class="flex h-60 justify-center border text-center">
+        <template #title>{{ heading }}</template>
+        <template #subtitle>{{ subheading }}</template>
+        <template #content>
+            <div class="flex flex-col gap-2">
+                <p class="text-3xl font-bold text-indigo-600">{{ value }} zł</p>
+                <p v-if="secondValue">
+                    Pozostało do zapłaty: <span class="text-red-600">{{ secondValue }} zł</span>
+                </p>
+            </div>
+        </template>
+    </Card>
+</template>
