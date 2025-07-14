229cf1bd61b35730e9323b296763412230dd95d8
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-14 20:54:45 +0200

    add conditional rendering of zl for price display

--- resources/js/components/EditableField.vue
+++ resources/js/components/EditableField.vue
@@ -2,7 +2,6 @@
 import InputText from '@/components/volt/InputText.vue';
 import Select from '@/components/volt/Select.vue';
 
-
 defineProps({
     label: String,
     isEditing: Boolean,
@@ -17,21 +16,22 @@ defineProps({
     },
 });
 
-
-
 const emit = defineEmits(['update:modelValue']);
 </script>
 
 <template>
     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
         <label class="text-sm/6 font-medium text-gray-900">{{ label }}</label>
-        <p v-if="!isEditing" class="mt-1 px-2 py-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">{{ modelValue }}</p>
+        <p v-if="!isEditing" class="mt-1 px-2 py-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {{ label === 'Cena' ? modelValue + ' zł' : modelValue }}
+        </p>
         <Select
             v-if="isEditing && type === 'select'"
             :options="options"
             :modelValue="modelValue"
             @update:modelValue="(val) => emit('update:modelValue', val)"
         />
+
         <InputText
             v-else-if="isEditing"
             type="text"
