ebfba9179b17084ad2bb423585faee86e650272d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-15 13:33:40 +0200

    setup single payment page

---
+++ resources/js/pages/payments/Show.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Payment} from '@/types/models';
+
+const { payment } = defineProps<{
+    payment: Payment
+}>();
+</script>
+
+<template>
+    <div class="flex items-center justify-between">
+        <h2>tetetetete</h2>
+    </div>
+</template>
+
+<style scoped></style>
