ff6391cb40f1b6fbc55fde7f464c6aa91b1a7c94
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-16 20:37:51 +0200

    extract datefield component to reuse across forms

---
+++ resources/js/components/DateField.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import FormField from '@/components/FormField.vue';
+import DatePicker from '@/components/volt/DatePicker.vue';
+
+const { id, label, modelValue, placeholder } = defineProps<{
+    id: string;
+    label: string;
+    modelValue: any;
+    placeholder: string;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: any): void;
+}>();
+</script>
+
+<template>
+    <FormField :id="id" :label="label">
+        <DatePicker
+            :modelValue="modelValue"
+            @update:modelValue="(value) => emit('update:modelValue', value)"
+            showIcon
+            iconDisplay="input"
+            :placeholder="placeholder"
+        />
+    </FormField>
+</template>
