7f033083f346e23adf39d4d968bd449fe9bfaab6
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-16 20:29:13 +0200

    create dedicated select field component to reuse across forms

---
+++ resources/js/pages/payments/SelectField.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import FormField from '@/pages/payments/FormField.vue';
+import { useAttrs } from 'vue';
+import Select from '@/components/volt/Select.vue';
+
+const { id, label, error, modelValue, options, optionLabel, optionValue, placeholder } = defineProps<{
+    id: string;
+    label: string;
+    error?: string;
+    modelValue: any;
+    options: any[];
+    optionLabel?: string;
+    optionValue?: any;
+    placeholder?: string;
+}>();
+
+const attrs = useAttrs();
+
+const emit = defineEmits<{
+    (event: 'update:modelValue', value: any): void;
+}>();
+</script>
+
+<template>
+    <FormField :id="id" :label="label" :error="error">
+        <Select
+            :modelValue="modelValue"
+            @update:modelValue="(value) => emit('update:modelValue', value)"
+            :options="options"
+            :optionLabel="optionLabel"
+            :optionValue="optionValue"
+            editable
+            :placeholder="placeholder"
+            required
+            v-bind="attrs"
+        />
+    </FormField>
+</template>
