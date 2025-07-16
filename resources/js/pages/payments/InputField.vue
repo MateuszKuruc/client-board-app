1a8e525d673a6777360e1d8c639703270f48a281
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-16 20:29:05 +0200

    create dedicated input field component to reuse across forms

---
+++ resources/js/pages/payments/InputField.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import InputText from '@/components/volt/InputText.vue';
+import FormField from '@/pages/payments/FormField.vue';
+
+const { id, label, error, modelValue, placeholder } = defineProps<{
+    id: string;
+    label: string;
+    error?: string;
+    modelValue: any;
+    placeholder?: string;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: any): void;
+}>();
+</script>
+
+<template>
+    <FormField :id="id" :label="label" :error="error">
+        <InputText :id="id" :modelValue="modelValue" @update:modelValue="(value) => emit('update:modelValue', value)" :placeholder="placeholder" />
+    </FormField>
+</template>
