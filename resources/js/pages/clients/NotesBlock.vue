4493fac6de8fee0b775e60d3c12acec0b2753e13
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-18 01:54:40 +0200

    setup reusable notes block

---
+++ resources/js/pages/clients/NotesBlock.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import SectionHeading from '@/components/SectionHeading.vue';
+import StyledLink from '@/components/StyledLink.vue';
+import { Plus } from 'lucide-vue-next';
+
+const props = withDefaults(defineProps<{
+    subheading?: string;
+    href: string;
+}>(), {
+    subheading: 'Sprawdź listę dodatkowych notatek'
+});
+</script>
+
+<template>
+    <div>
+        <div class="flex items-center justify-between">
+            <SectionHeading heading="Notatki" :subheading="subheading" />
+            <StyledLink variant="outline" :href="href">
+                <Plus class="w-5 text-gray-400" />
+                Dodaj notatkę
+            </StyledLink>
+        </div>
+    </div>
+</template>
