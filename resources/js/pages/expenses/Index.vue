3276f21796f38e6bb7ea961e682e35cbcfc23e81
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-17 12:52:34 +0200

    setup basic index component to display paginated expeneses data

---
+++ resources/js/pages/expenses/Index.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import DataTableToolbar from '@/components/DataTableToolbar.vue';
+import Paginator from '@/components/Paginator.vue';
+import StyledLink from '@/components/StyledLink.vue';
+import DataTable from '@/components/volt/DataTable.vue';
+import Tag from '@/components/volt/Tag.vue';
+import { useServerSearch } from '@/composables/useServerSearch';
+import AppLayout from '@/layouts/AppLayout.vue';
+import type { BreadcrumbItem } from '@/types';
+import type { Filters, Lead, Paginated, Expense } from '@/types/models';
+import { Head } from '@inertiajs/vue3';
+import { SquarePen } from 'lucide-vue-next';
+import Column from 'primevue/column';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Koszty',
+        href: '/koszty',
+    },
+];
+
+const { expenses, filters } = defineProps<{
+    expenses: Paginated<Expense>;
+    filters: Filters;
+}>();
+
+const { globalSearch } = useServerSearch(filters.search || '', 'expenses.index');
+</script>
+
+<template>
+    <Head title="Leady" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <DataTable :value="expenses.data" dataKey="id">
+                <template #header>
+                    <DataTableToolbar v-model="globalSearch" :exportUrl="'leads.export'" :filters="filters" />
+                </template>
+                <Column field="id" header="Numer płatności" />
+                <Column field="name" header="Koszt" />
+                <Column field="type" header="Rodzaj płatności">
+                    <template #body="{ data: expense }: { data: Expense }">
+                        {{ expense.type }}
+                    </template>
+                </Column>
+                <Column field="payment_date" header="Data płatności" />
+
+                <Column header="Opcje">
+                    <template #body="{ data: expense }: { data: Expense }">
+                        <StyledLink variant="text" href="#">
+                            <SquarePen />
+                            Edytuj
+                        </StyledLink>
+                    </template>
+                </Column>
+            </DataTable>
+            <Paginator :data="expenses" />
+        </div>
+    </AppLayout>
+</template>
