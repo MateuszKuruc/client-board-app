4a1aa56d833e0cf127c2c795e1dc0a0b90cd6d54
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-17 11:09:42 +0200

    setup all form fields specific to project and add inertia transform for handling date

--- resources/js/pages/projects/Create.vue
+++ resources/js/pages/projects/Create.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import DateField from '@/components/DateField.vue';
 import FormLayout from '@/components/FormLayout.vue';
 import InputField from '@/components/InputField.vue';
 import SelectField from '@/components/SelectField.vue';
 import SubmitButton from '@/components/SubmitButton.vue';
-import { locationOptions } from '@/constants/locationOptions';
-import { sourceOptions } from '@/constants/sourceOptions';
+import { booleanOptions } from '@/constants/booleanOptions';
+import { projectTypeOptions } from '@/constants/projectTypeOptions';
 import AppLayout from '@/layouts/AppLayout.vue';
+import dayjs from '@/plugins/dayjs';
 import type { BreadcrumbItem } from '@/types';
 import { Head, useForm } from '@inertiajs/vue3';
 import { useToast } from 'primevue/usetoast';
@@ -47,7 +49,15 @@ const form = useForm<Payment>({
 const submit = () => {
     form.clearErrors();
 
-    form.post(route('projects.store'), {
+    form.transform((data) => {
+        if (data.payment_date) {
+            data.payment_date = dayjs(data.payment_date).format('YYYY-MM-DD');
+        } else {
+            data.payment_date = null;
+        }
+
+        return data;
+    }).post(route('projects.store'), {
         onSuccess: () => {
             form.reset();
             toast.add({ severity: 'success', summary: 'Projekt dodany poprawnie', detail: 'Dane zostały zapisane w systemie', life: 3000 });
@@ -67,48 +77,66 @@ const submit = () => {
             <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <form @submit.prevent="submit" class="flex flex-col gap-6">
                     <div class="grid gap-6">
-                        <InputField id="name" label="Nazwa" :error="form.errors.name" v-model="form.name" placeholder="Jan Kowalski" required />
-
-                        <InputField
-                            id="email"
-                            label="Email"
-                            :error="form.errors.email"
-                            v-model="form.email"
-                            placeholder="example@gmail.com"
-                            required
-                        />
-
-                        <InputField id="phone" label="Telefon" :error="form.errors.phone" v-model="form.phone" placeholder="531380713" />
-
-                        <InputField id="nip" label="NIP" :error="form.errors.nip" v-model="form.nip" placeholder="7941840960" required />
+                        <InputField id="name" label="Nazwa" :error="form.errors.name" v-model="form.name" placeholder="Kampania reklamowa" required />
 
                         <SelectField
-                            id="source"
-                            label="Źródło"
-                            :error="form.errors.source"
-                            v-model="form.source"
-                            :options="sourceOptions"
-                            placeholder="Wybierz źródło"
-                            required
-                        />
-
-                        <SelectField
-                            id="location"
-                            label="Lokalizacja"
-                            :error="form.errors.location"
-                            v-model="form.location"
-                            :options="locationOptions"
+                            id="client_id"
+                            label="Klient"
+                            :error="form.errors.client_id"
+                            v-model="form.client_id"
+                            :options="clients"
                             optionLabel="label"
                             optionValue="value"
-                            placeholder="Wybierz lokalizację"
+                            placeholder="Wybierz klienta"
                             required
                         />
+
+                        <SelectField
+                            id="service_id"
+                            label="Usługa"
+                            :error="form.errors.service_id"
+                            v-model="form.service_id"
+                            :options="services"
+                            optionLabel="label"
+                            optionValue="value"
+                            placeholder="Wybierz usługę"
+                            required
+                        />
+
+                        <SelectField
+                            id="active"
+                            label="Status"
+                            :error="form.errors.active"
+                            v-model="form.active"
+                            :options="booleanOptions"
+                            optionLabel="label"
+                            optionValue="value"
+                            placeholder="Wybierz status projektu"
+                            required
+                        />
+
+                        <InputField id="price" label="Cena" :error="form.errors.price" v-model="form.price" placeholder="1000" required />
+
+                        <SelectField
+                            id="type"
+                            label="Rodzaj"
+                            :error="form.errors.type"
+                            v-model="form.type"
+                            :options="projectTypeOptions"
+                            placeholder="Rodzaj projektu"
+                            required
+                        />
+
+                        <DateField id="start_date" label="Data startu" v-model="form.start_date" placeholder="Data rozpoczęcia projektu" />
+                        <DateField id="end_date" label="Data zakończenia" v-model="form.end_date" placeholder="Data zakończenia projektu" />
 
                         <SubmitButton
                             :processing="form.processing"
-                            buttonLabel="Utwórz profil"
+                            buttonLabel="Utwórz projekt"
                             loadingLabel="Zapisywanie..."
-                            :disabled="form.processing || !form.name || !form.email || !form.nip || !form.source || !form.location"
+                            :disabled="
+                                form.processing || !form.name || !form.client_id || !form.service_id || !form.active || !form.price || !form.type
+                            "
                         />
                     </div>
                 </form>
