e6a3848b117634a097585356333e3f1414e33054
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-17 12:44:22 +0200

    setup index, create and store methods for expenses

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Expenses\StoreExpenseRequest;
+use App\Models\Expense;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ExpenseController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+
+        $expenses = Expense::when($search, function ($query, $search) {
+            $query->where('name', 'like', '%'.$search.'%')
+                ->orWhere('type', 'like', '%'.$search.'%');
+        })
+            ->orderBy('payment_date', 'desc')
+            ->paginate(10)
+            ->withQueryString();;
+
+        return Inertia::render('expenses/Index', [
+            'expenses' => $expenses
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('expenses/Create');
+    }
+
+    public function store(StoreExpenseRequest $request)
+    {
+        $validated = $request->validated();
+
+        Expense::create($validated);
+
+        return redirect()->route('expenses.index');
+    }
 }
+
