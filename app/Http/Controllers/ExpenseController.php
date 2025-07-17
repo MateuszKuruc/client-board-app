6a638ce20b2380022a9520cf5d172e1b296c5dfb
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-17 17:51:56 +0200

    add show, update and export methods for expense controller

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\ProjectsExport;
 use App\Http\Requests\Expenses\StoreExpenseRequest;
 use App\Models\Expense;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ExpenseController extends Controller
 {
@@ -41,6 +43,29 @@ class ExpenseController extends Controller
         Expense::create($validated);
 
         return redirect()->route('expenses.index');
+    }
+
+    public function show(Expense $expense)
+    {
+        return Inertia::render('expenses/Show', [
+            'expense' => $expense
+        ]);
+    }
+
+    public function update(StoreExpenseRequest $request, Expense $expense)
+    {
+        $validated = $request->validated();
+
+        $expense->update($validated);
+
+        return redirect()->route('expenses.show', $expense->id);
+    }
+
+    public function export(Request $request)
+    {
+        $search = $request->input('search');
+
+        return Excel::download(new ProjectsExport($search), 'expenses.xlsx');
     }
 }
 
