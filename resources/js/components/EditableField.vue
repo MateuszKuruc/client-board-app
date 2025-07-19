1a093dfb7508a9a82bdc1aa86d4988b6dbb2dd9c
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-19 13:29:28 +0200

    add handling for taking props for multiselect and displaying correctly. Change input sizes

--- resources/js/components/EditableField.vue
+++ resources/js/components/EditableField.vue
@@ -2,40 +2,27 @@
 import DatePicker from '@/components/volt/DatePicker.vue';
 import InputText from '@/components/volt/InputText.vue';
 import Message from '@/components/volt/Message.vue';
+import MultiSelect from '@/components/volt/MultiSelect.vue';
 import Select from '@/components/volt/Select.vue';
 import { viewLabels } from '@/constants/viewLabels';
 import dayjs from '@/plugins/dayjs';
-import { PropType } from 'vue';
+import { User } from 'lucide-vue-next';
+import { PropType, computed } from 'vue';
 
 interface EditableFieldProps {
     label: string;
     isEditing: boolean;
-    modelValue: string | Date;
-    type?: 'text' | 'select' | 'picker';
+    modelValue: string | Date | Array<string | number>;
+    type?: 'text' | 'select' | 'picker' | 'multiselect';
     options?: string[] | { value: string; label: string }[];
     minDate?: string | Date;
     error?: string[] | null;
 }
 
-// const { label, isEditing, modelValue, type, options, minDate } = defineProps({
-//     label: String as PropType<EditableFieldProps['label']>,
-//     isEditing: Boolean as PropType<EditableFieldProps['isEditing']>,
-//     modelValue: [String, Date] as PropType<EditableFieldProps['modelValue']>,
-//     type: {
-//         type: String as PropType<EditableFieldProps['type']>,
-//         default: 'text',
-//     },
-//     options: {
-//         type: Array as PropType<EditableFieldProps['options']>,
-//         default: () => [] as string[],
-//     },
-//     minDate: [String, Date] as PropType<EditableFieldProps['minDate']>,
-// } as const);
-
 const props = defineProps({
     label: String as PropType<EditableFieldProps['label']>,
     isEditing: Boolean as PropType<EditableFieldProps['isEditing']>,
-    modelValue: [String, Date] as PropType<EditableFieldProps['modelValue']>,
+    modelValue: [String, Date, Array] as PropType<EditableFieldProps['modelValue']>,
     type: {
         type: String as PropType<EditableFieldProps['type']>,
         default: 'text',
@@ -46,7 +33,7 @@ const props = defineProps({
     },
     minDate: [String, Date] as PropType<EditableFieldProps['minDate']>,
     error: {
-        type: Array as PropType<string[]>,
+        type: [String, Array] as PropType<string | string[]>,
         default: () => [],
     },
 } as const);
@@ -54,6 +41,12 @@ const props = defineProps({
 const emit = defineEmits<{
     (e: 'update:modelValue', value: string): void;
 }>();
+
+const multiselectLabels = computed<string[]>(() => {
+    if (props.type === 'multiselect' && Array.isArray(props.modelValue) && Array.isArray(props.options)) {
+        return props.options.filter((o) => props.modelValue.includes(o.value)).map((o) => o.label);
+    }
+});
 </script>
 
 <template>
@@ -67,6 +60,10 @@ const emit = defineEmits<{
             {{ viewLabels[modelValue] || modelValue }}
         </p>
 
+        <p v-if="!isEditing && type === 'multiselect'" class="mt-1 px-2 py-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {{ multiselectLabels.join(', ') }}
+        </p>
+
         <p v-else-if="!isEditing && type === 'picker'" class="mt-1 px-2 py-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
             {{ modelValue ? dayjs(modelValue).format('DD.MM.YYYY') : '-' }}
         </p>
@@ -78,8 +75,31 @@ const emit = defineEmits<{
                 :optionLabel="typeof options[0] === 'string' ? undefined : 'label'"
                 :optionValue="typeof options[0] === 'string' ? undefined : 'value'"
                 :modelValue="modelValue"
+                size="small"
                 @update:modelValue="(val) => emit('update:modelValue', val)"
             />
+            <Message v-if="props.error.length" severity="error" size="small">{{ error }}</Message>
+        </div>
+
+        <div v-else-if="isEditing && type === 'multiselect'" class="flex flex-col gap-2">
+            <MultiSelect
+                v-if="isEditing && type === 'multiselect'"
+                :options="options"
+                optionLabel="label"
+                optionValue="value"
+                :modelValue="modelValue"
+                display="chip"
+                showClear
+                filter
+                size="small"
+                placeholder="Przypisz opiekuna projektu"
+                selectAllLabel="sss"
+                @update:modelValue="(val) => emit('update:modelValue', val)"
+            >
+                <template #dropdownicon>
+                    <User />
+                </template>
+            </MultiSelect>
             <Message v-if="props.error.length" severity="error" size="small">{{ error }}</Message>
         </div>
 
@@ -91,6 +111,7 @@ const emit = defineEmits<{
                 dateFormat="dd.mm.yy"
                 :minDate="minDate"
                 :modelValue="modelValue"
+                size="small"
                 @update:modelValue="(val) => emit('update:modelValue', val)"
             />
             <Message v-if="props.error.length" severity="error" size="small">{{ error }}</Message>
@@ -100,6 +121,7 @@ const emit = defineEmits<{
             <InputText
                 type="text"
                 :value="modelValue"
+                size="small"
                 @input="(e) => emit('update:modelValue', e.target.value)"
                 class="mt-1 px-2 py-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0"
             />
