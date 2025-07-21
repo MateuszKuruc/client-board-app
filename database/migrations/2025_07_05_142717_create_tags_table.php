57ea13f96fecc2d870ecb9c9bcc92d605a4cd119
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-22 01:58:18 +0200

    add migration for tag polymorphic pivot

--- database/migrations/2025_07_05_142717_create_tags_table.php
+++ database/migrations/2025_07_05_142717_create_tags_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('severity', ['primary', 'secondary', 'success', 'info', 'warn', 'danger', 'contrast']);
             $table->timestamps();
         });
     }
