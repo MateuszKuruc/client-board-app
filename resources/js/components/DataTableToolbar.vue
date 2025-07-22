88321a39b637097bf89227e42835407529b23b29
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-23 01:15:36 +0200

    add ts support, add new props

--- resources/js/components/DataTableToolbar.vue
+++ resources/js/components/DataTableToolbar.vue
@@ -1,39 +1,34 @@
-<script setup>
+<script setup lang="ts">
 import Button from '@/components/volt/Button.vue';
 import InputText from '@/components/volt/InputText.vue';
 import SecondaryButton from '@/components/volt/SecondaryButton.vue';
-import { ChevronsDown, ChevronsUp, Search, FileDown } from 'lucide-vue-next';
+import { ChevronsDown, ChevronsUp, FileDown, Search } from 'lucide-vue-next';
 
-defineProps({
-    modelValue: String,
-    onExpandAll: Function,
-    onCollapseAll: Function,
-    exportUrl: String,
-    filters: Object,
-    searchable: {
-        type: Boolean,
-        default: true,
-    },
-    expandLabel: {
-        type: String,
-        default: 'Rozwiń wszystkie',
-    },
-    collapseLabel: {
-        type: String,
-        default: 'Zwiń wszystkie',
-    },
-    exportLabel: {
-        type: String,
-        default: 'Eksportuj do CSV',
-    },
-});
+interface exportParams {
+    search: string;
+    sort_by: string;
+    sort_dir: string;
+}
+
+const props = defineProps<{
+    modelValue: string;
+    onExpandAll?: () => void;
+    onCollapseAll?: () => void;
+    exportUrl: string;
+    filters: Record<string, any>;
+    searchable?: boolean;
+    expandLabel?: string;
+    collapseLabel?: string;
+    exportLabel?: string;
+    exportParams?: ExportParams;
+}>();
 
 const emit = defineEmits(['update:modelValue']);
 </script>
 
 <template>
     <div class="flex flex-col-reverse justify-between gap-4 py-4 xl:flex-row xl:py-8">
-        <div class="flex gap-2" :class="{ 'invisible': !onExpandAll && !onCollapseAll }">
+        <div class="flex gap-2" :class="{ invisible: !onExpandAll && !onCollapseAll }">
             <SecondaryButton @click="onExpandAll?.()">
                 <ChevronsDown />
                 {{ expandLabel }}
@@ -42,18 +37,21 @@ const emit = defineEmits(['update:modelValue']);
                 <ChevronsUp />
                 {{ collapseLabel }}
             </SecondaryButton>
-            <!--                            <Button @click="expandAll"><ChevronsDown /> Rozwiń wszystkie </Button>-->
-            <!--                            <Button @click="collapseAll"><ChevronsUp /> Zwiń wszystkie</Button>-->
         </div>
         <div class="relative flex items-center gap-2">
             <div class="relative">
-
-            <Search v-if="searchable" class="absolute top-1/2 -mt-3 text-surface-400 leading-none start-3 z-1" />
-            <InputText v-if="searchable" :modelValue="modelValue" @update:modelValue="emit('update:modelValue', $event)" placeholder="Szukaj" pt:root="ps-10" />
+                <Search v-if="searchable" class="absolute start-3 top-1/2 z-1 -mt-3 leading-none text-surface-400" />
+                <InputText
+                    v-if="searchable"
+                    :modelValue="modelValue"
+                    @update:modelValue="emit('update:modelValue', $event)"
+                    placeholder="Szukaj"
+                    pt:root="ps-10"
+                />
             </div>
-            <Button as="a" :href="route(exportUrl, { search: filters.search })">
+            <Button as="a" :href="route(exportUrl, exportParams)">
                 <FileDown />
-                {{ exportLabel }}
+                {{ exportLabel ? exportLabel : 'Eksportuj dane' }}
             </Button>
         </div>
     </div>
