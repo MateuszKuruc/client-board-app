0cb394d08ee1e6c391f89115475ab54815f83769
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-22 02:09:23 +0200

    prepare form request for tag updates across controllers

---
+++ app/Http/Requests/Tags/SyncTagsRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Tags;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+
+class SyncTagsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+
+    public function authorize()
+    {
+        // you can add your authorization logic here
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'tags'   => 'required|array',
+            'tags.*' => 'integer|exists:tags,id',
+        ];
+    }
+}
