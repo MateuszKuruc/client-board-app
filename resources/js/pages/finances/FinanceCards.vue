e732552c080698337a207170232d78abd3984295
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-24 20:37:31 +0200

    setup cards component for finances page

---
+++ resources/js/pages/finances/FinanceCards.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ReusableCard from '@/components/ReusableCard.vue';
+
+const props = defineProps<{
+    firstHeading: string;
+    secondHeading: string;
+    value: number;
+    percentage: number;
+}>();
+</script>
+
+<template>
+    <div class="my-8 grid grid-cols-2 gap-4">
+        <ReusableCard :heading="firstHeading" :value="value" />
+        <ReusableCard :heading="secondHeading" :percentage="percentage" />
+    </div>
+</template>
