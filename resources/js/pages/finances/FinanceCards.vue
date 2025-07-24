5543fb162a209bed36e4d8dde59749b6e30ca21f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-24 21:21:55 +0200

    refactor to display summary

--- resources/js/pages/finances/FinanceCards.vue
+++ resources/js/pages/finances/FinanceCards.vue
@@ -4,14 +4,15 @@ import ReusableCard from '@/components/ReusableCard.vue';
 const props = defineProps<{
     firstHeading: string;
     secondHeading: string;
-    value: number;
+    value?: number;
     percentage: number;
+    summary?: number;
 }>();
 </script>
 
 <template>
     <div class="my-8 grid grid-cols-2 gap-4">
-        <ReusableCard :heading="firstHeading" :value="value" />
+        <ReusableCard :heading="firstHeading" :value="value" :summary="summary" />
         <ReusableCard :heading="secondHeading" :percentage="percentage" />
     </div>
 </template>
