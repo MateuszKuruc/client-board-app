3d1d371caf041749e67270d3403b95314348eae0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-24 18:25:33 +0200

    setup finance controller to pass filtered data to frontend based on the request params

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
+use App\Models\Payment;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Carbon\Carbon;
 
 class FinanceController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $month = $request->input('month', now()->format('Y-m'));
+
+        $current = Carbon::createFromFormat('Y-m', $month);
+
+        $year = $current->format('Y');
+        $monthNum = $current->format('m');
+
+
+        $paymentsQuery = Payment::with('project.client')
+            ->whereYear('payment_date', $year)
+            ->whereMonth('payment_date', $monthNum);
+
+        $expensesQuery = Expense::whereYear('payment_date', $year)
+            ->whereMonth('payment_date', $monthNum);
+
+        $payments = $paymentsQuery->paginate(10);
+        $expenses = $expensesQuery->paginate(10);
+
+        $totalPayments = $paymentsQuery->sum('amount');
+        $totalExpenses = $expensesQuery->sum('amount');
+
+        $prev = $current->copy()->subMonth();
+        $prevYear = $prev->format('Y');
+        $prevMonth = $prev->format('m');
+
+        $previousTotalPayments = Payment::with('project.client')
+            ->whereYear('payment_date', $prevYear)
+            ->whereMonth('payment_date', $prevMonth)
+            ->sum('amount');
+
+        $previousTotalExpenses = Expense::whereYear('payment_date', $prevYear)
+            ->whereMonth('payment_date', $prevMonth)
+            ->sum('amount');
+
+        $changeInPayments = ($totalPayments && $previousTotalPayments) ?
+            round((($totalPayments / $previousTotalPayments) - 1) * 100, 2) : null;
+
+        $changeInExpenses = ($totalExpenses && $previousTotalExpenses) ?
+            round((($totalExpenses / $previousTotalExpenses) - 1) * 100, 2) : null;
+
+//        $changeInSummary
+
+        return Inertia::render('finances/Index', [
+            'month' => $month,
+            'payments' => $payments,
+            'expenses' => $expenses,
+            'totalPayments' => $totalPayments,
+            'totalExpenses' => $totalExpenses,
+            'changeInPayments' => $changeInPayments,
+            'changeInExpenses' => $changeInExpenses,
+        ]);
+    }
 }
