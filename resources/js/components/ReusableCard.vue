0eb664442ac53e35b1a36747572c86e159041329
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-24 18:05:16 +0200

    add optional percentage props, change text color based on percentage change

--- resources/js/components/ReusableCard.vue
+++ resources/js/components/ReusableCard.vue
@@ -4,8 +4,9 @@ import Card from '@/components/volt/Card.vue';
 const { value } = defineProps<{
     heading: string;
     subheading?: string;
-    value: number;
+    value?: number;
     secondValue?: number;
+    percentage?: number;
 }>();
 </script>
 
@@ -15,7 +16,10 @@ const { value } = defineProps<{
         <template #subtitle>{{ subheading }}</template>
         <template #content>
             <div class="flex flex-col gap-2">
-                <p class="text-3xl font-bold text-indigo-600">{{ value }} zł</p>
+                <p v-if="value" class="text-3xl font-bold text-indigo-600">{{ value }} zł</p>
+                <p v-if="percentage" class="text-3xl font-bold">
+                    <span :class="percentage > 0 ? 'text-green-500' : 'text-red-500'"> {{ percentage }} % </span>
+                </p>
                 <p v-if="secondValue">
                     Pozostało do zapłaty: <span class="text-red-600">{{ secondValue }} zł</span>
                 </p>
