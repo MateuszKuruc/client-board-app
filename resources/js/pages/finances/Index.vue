3f1c3072d352eefbb9ff90f40efb39bdb4bf4b0c
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-25 02:20:25 +0200

    build summary section with charts

--- resources/js/pages/finances/Index.vue
+++ resources/js/pages/finances/Index.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
-import Paginator from '@/components/Paginator.vue';
+import BarChart from '@/components/BarChart.vue';
 import ReusableCard from '@/components/ReusableCard.vue';
-import StyledLink from '@/components/StyledLink.vue';
-import DataTable from '@/components/volt/DataTable.vue';
+import Fieldset from '@/components/volt/Fieldset.vue';
 import SelectButton from '@/components/volt/SelectButton.vue';
 import Tab from '@/components/volt/Tab.vue';
 import TabList from '@/components/volt/TabList.vue';
 import { default as TabPanel, default as TabPanels } from '@/components/volt/TabPanels.vue';
 import Tabs from '@/components/volt/Tabs.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
+import FinanceCards from '@/pages/finances/FinanceCards.vue';
+import FinanceTable from '@/pages/finances/FinanceTable.vue';
 import dayjs from '@/plugins/dayjs';
 import type { BreadcrumbItem } from '@/types';
 import { Expense, Paginated, Payment } from '@/types/models';
 import { Head, router } from '@inertiajs/vue3';
-import { SquarePen } from 'lucide-vue-next';
-import Column from 'primevue/column';
 import { computed, ref, watch } from 'vue';
 
 const props = defineProps<{
     month: string;
+    tab?: string;
     payments: Paginated<Payment>;
     expenses: Paginated<Expense>;
     totalPayments: number;
     totalExpenses: number;
+    summary: number;
     changeInPayments: number;
     changeInExpenses: number;
+    changeInSummary: number;
+    activeSubsValue: number;
+    activeProjects: number;
 }>();
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -34,8 +38,8 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const optionValues = ref(['Wpływy', 'Wydatki', 'Zestawienie']);
-const selectValue = ref(optionValues.value[0]);
+const optionValues = ref(['Podsumowanie', 'Wpływy', 'Wydatki']);
+const selectValue = ref(props.tab || optionValues.value[0]);
 
 const startMonth = dayjs('2024-03-01');
 const now = dayjs();
@@ -55,9 +59,26 @@ const scrollableTabs = computed(() => {
 const selectedMonth = ref(props.month);
 
 watch(
+    () => selectValue.value,
+    (newValue) => {
+        router.get(
+            route('finances.index', {
+                month: selectedMonth.value,
+                tab: newValue,
+            }),
+            {},
+            {
+                preserveState: true,
+                preserveScroll: true,
+            },
+        );
+    },
+);
+
+watch(
     () => selectedMonth.value,
     () => {
-        router.get(route('finances.index', { month: selectedMonth.value }));
+        router.get(route('finances.index', { month: selectedMonth.value, tab: selectValue.value }));
     },
 );
 </script>
@@ -67,46 +88,90 @@ watch(
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-            <div class="card">
+            <div>
+                <Fieldset legend="Statystyki" toggleable>
+                    <div class="mx-8 my-4 grid grid-cols-3 gap-4">
+                        <ReusableCard heading="Aktualna wartość subskrypcji" :value="activeSubsValue" class="h-fit" />
+                        <ReusableCard heading="Średnie wpływy z 3 miesięcy" :value="activeSubsValue" class="h-fit" />
+                        <ReusableCard heading="Liczba aktywnych projektów" :plainNumber="activeProjects" class="h-fit" />
+                    </div>
+                </Fieldset>
+
                 <Tabs v-model:value="selectedMonth" scrollable>
-                    <TabList>
+                    <TabList class="mt-8">
                         <Tab v-for="month in scrollableTabs" :key="month" :value="month">{{ month }}</Tab>
                     </TabList>
 
                     <TabPanels>
                         <TabPanel class="space-y-2">
                             <SelectButton v-model="selectValue" :options="optionValues" />
+                            <div v-if="selectValue === optionValues[0]">
+                                <FinanceCards
+                                    firstHeading="Podsumowanie miesiąca"
+                                    secondHeading="W porównaniu do zeszłego miesiąca"
+                                    :summary="summary"
+                                    :percentage="changeInSummary"
+                                />
 
-                            <div v-if="selectValue === 'Wpływy'">
-                                <div class="my-8 grid grid-cols-2 gap-4">
-                                    <ReusableCard heading="Wpływy miesięczne" :value="totalPayments" />
-                                    <ReusableCard heading="W porównaniu z poprzednim miesiącem" :percentage="changeInPayments" />
+                                <div class="flex flex-col gap-24">
+                                    <div class="grid grid-cols-2">
+                                        <BarChart
+                                            type="pie"
+                                            :labels="['test', 'test2', 'test3']"
+                                            :values="[223, 2323, 23154]"
+                                            class="max-w-[500px]"
+                                            mainLabel="Udział subskrypcji we wszystkich projektach"
+                                            secondaryLabel="Liczba projektów"
+                                        />
+
+                                        <Fieldset legend="Miesiąc w liczbach" class="h-fit">
+                                            <div class="flex flex-col justify-between gap-4 p-4">
+                                                <ReusableCard heading="Subskrypcje stanowiły" :percentage="activeSubsValue" class="h-fit" />
+                                                <ReusableCard heading="Udział miesiąc do miesiąca" :percentage="activeSubsValue" class="h-fit" />
+                                                <ReusableCard heading="Średnia opłata za projekt" :value="activeSubsValue" class="h-fit" />
+                                            </div>
+                                        </Fieldset>
+                                    </div>
+
+                                    <div class="grid grid-cols-2">
+                                        <Fieldset legend="Dodatkowe statystyki">
+                                            <div class="flex flex-col justify-between gap-4 p-4">
+                                                <ReusableCard heading="Najwyższa subskrypcja" :value="activeSubsValue" class="h-fit" />
+                                                <ReusableCard heading="Średnia kwota subskrypcji" :value="activeSubsValue" class="h-fit" />
+                                                <ReusableCard heading="Średnia kwota standardowego projektu" :value="activeSubsValue" class="h-fit" />
+                                            </div>
+                                        </Fieldset>
+
+                                        <BarChart
+                                            type="doughnut"
+                                            :labels="['test', 'test2', 'test3']"
+                                            :values="[223, 2323, 23154]"
+                                            class="max-w-[500px]"
+                                            mainLabel="Wysokość zysków z subskrybcji"
+                                            secondaryLabel="Zapłacona kwota"
+                                        />
+                                    </div>
                                 </div>
+                            </div>
 
-                                <DataTable :value="payments.data" dataKey="id">
-                                    <Column field="id" header="Nr płatności" />
-                                    <Column field="amount" header="Kwota" />
-                                    <Column field="project.name" header="Projekt" />
-                                    <Column field="project.client.name" header="Klient" />
-                                    <Column field="payment_date" header="Data płatności" />
-                                    <Column>
-                                        <template #body="{ data }: { data: Payment }">
-                                            <StyledLink
-                                                variant="default"
-                                                :href="
-                                                    route('payments.show', {
-                                                        client: data.project.client.slug,
-                                                        project: data.project.id,
-                                                        payment: data.id,
-                                                    })
-                                                "
-                                            >
-                                                <SquarePen />
-                                            </StyledLink>
-                                        </template>
-                                    </Column>
-                                </DataTable>
-                                <Paginator :data="payments" />
+                            <div v-if="selectValue === optionValues[1]">
+                                <FinanceCards
+                                    firstHeading="Wpływy miesięczne"
+                                    secondHeading="W porównaniu do zeszłego miesiąca"
+                                    :value="totalPayments"
+                                    :percentage="changeInPayments"
+                                />
+                                <FinanceTable :data="payments" />
+                            </div>
+
+                            <div v-if="selectValue === optionValues[2]">
+                                <FinanceCards
+                                    firstHeading="Wydatki miesięczne"
+                                    secondHeading="W porównaniu do zeszłego miesiąca"
+                                    :value="totalExpenses"
+                                    :percentage="changeInExpenses"
+                                />
+                                <FinanceTable :data="expenses" />
                             </div>
                         </TabPanel>
                     </TabPanels>
