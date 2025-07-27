1bfe697d614ec5f2a74f14ce2e5ef8f570a7cb42
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-27 22:19:25 +0200

    add option to add tags and choose their severity

--- resources/js/components/SectionHeading.vue
+++ resources/js/components/SectionHeading.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
-import Divider from '@/components/volt/Divider.vue';
 import SingleHeading from '@/components/SingleHeading.vue';
 import SingleSubheading from '@/components/SingleSubheading.vue';
+import Divider from '@/components/volt/Divider.vue';
+import Tag from '@/components/volt/Tag.vue';
 
 const { heading, subheading, divider } = defineProps<{
     heading: string;
     subheading?: string;
     divider?: boolean;
+    tag?: string;
+    severity?: string;
 }>();
 </script>
 
 <template>
-    <div class="px-4 sm:px-0 my-8">
-        <SingleHeading :heading="heading" />
+    <div class="my-8 px-4 sm:px-0">
+        <div class="flex gap-2">
+            <Tag v-if="tag" :severity="severity">{{ tag.toUpperCase() }}</Tag>
+            <SingleHeading :heading="heading" />
+        </div>
         <SingleSubheading :subheading="subheading" />
         <Divider v-if="divider" />
     </div>
