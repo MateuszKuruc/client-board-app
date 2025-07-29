29ff334e0fccd50bbf27341b74b9995f10dda1d4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-29 20:28:32 +0200

    setup note model and controller

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class NoteController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'user_id' => 'required',
+            'noteable_type' => 'required',
+            'noteable_id' => 'required',
+            'content' => 'required',
+            'edited_at' => 'nullable'
+        ]);
+
+        Note::create($validated);
+
+        return redirect()->back();
+    }
+
+    public function destroy(Note $note)
+    {
+
+        Log::info('Delete attempt', [
+            'note_user_id' => $note->user_id,
+            'note_user_id_type' => gettype($note->user_id),
+            'auth_id' => auth()->id(),
+            'auth_id_type' => gettype(auth()->id()),
+            'are_equal_loose' => $note->user_id == auth()->id(),
+            'are_equal_strict' => $note->user_id === auth()->id(),
+        ]);
+
+        if ($note->user_id !== auth()->id()) {
+            abort(403, 'Możesz usunąć tylko swoje notatki');
+        }
+
+        $note->delete();
+
+        return redirect()->back();
+    }
 }
--- app/Models/Note.php
+++ app/Models/Note.php
@@ -4,9 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Note extends Model
 {
     /** @use HasFactory<\Database\Factories\NoteFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'noteable_type',
+        'noteable_id',
+        'content',
+        'edited_at'
+    ];
+
+    public function noteable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
